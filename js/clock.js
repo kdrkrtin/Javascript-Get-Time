a8c4765b538f1028f3cb921fc5121a1b3183b944
kdrkrtin <kadir.kartin06@hotmail.com>
2022-06-07 17:39:13 +0300

    javascript use date function

---
+++ js/clock.js
@@ -0,0 +1,17 @@
+// let userName = prompt('Lütfen İsminizi Giriniz');
+let nameTemp = document.querySelector('span#myName');
+let daysTemp = document.querySelector('div.days');
+let hoursTemp = document.querySelector('div.hours');
+let days = ['Pazartesi', 'Salı', 'Çarşamba', 'Perşembe', 'Cuma', 'Cumartesi', 'Pazar'];
+let months = ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylül', 'Ekim', 'Kasım', 'Aralık'];
+let dayIndex = new Date().getDay();
+nameTemp.innerHTML = 'userName';
+
+function showTime() {
+    hoursTemp.innerHTML = `${new Date().getDate()} ${months[new Date().getMonth()]} ${new Date().getFullYear()}`
+    daysTemp.innerHTML = `${days[dayIndex - 1]}`
+}
+
+setInterval(()=>{
+    showTime();
+}, 1000)
